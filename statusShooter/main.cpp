67caa4143a0f88cc8e6cd31f19053b9074adf396
overkiller <dkpolan3@gmail.com>
2015-03-02 10:16:15 +0100

    daemon skeleton test

--- statusShooter/main.cpp
+++ statusShooter/main.cpp
@@ -8,19 +8,55 @@ Read config from file (standard path)
 #include "CTemperature.h"
 #include <glog/logging.h>
 #include "CStatusShooter.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+pid_t pid, sid;
+void forkOff()
+{
+	pid = fork();
+	if(pid < 0)
+	{
+		exit(-1);
+	}
+	if(pid > 0)
+	{
+		exit(0);
+	}
+	umask(0);
+	sid = setsid();
+	// google::InitGoogleLogging(argv[0]);
+	if(sid < 0)
+	{
+		// LOG(ERROR) << "Cannot set sid, exiting";
+		exit(-1);
+	}
+	if((chdir("/")) < 0)
+	{
+		// LOG(ERROR) << "Cannot change diretctory to /";
+		exit(-1);
+	}
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+        
+}
 int main(int argc, char const *argv[])
 {
-	google::InitGoogleLogging(argv[0]);
+	forkOff();
 	CCpu processorData;
 	CRam ramData;
 	CSwap swapData;
 	CTemperature temperatureData;
 	CStatusShooter shooter;
 	int status = 0;
-	ramData.getData('N');
-	swapData.getData('N');
-	temperatureData.getTemperatureData('N');
-	processorData.ReadCpuData('N');
 	while(true)
 	{
 		shooter.addToSql(processorData.GetCpuFreqString(), ramData.getRamDataCapacity(), temperatureData.GetmStringTemp(),swapData.GetmData());
